b3985bf80a9efcac8143be66fa1c54ea889a650b
Mayank Singh <321mayanksingh@gmail.com>
2025-09-02 16:59:57 +0530

    feat: router

--- app/core/routers/__init__.py
+++ app/core/routers/__init__.py
@@ -6,13 +6,17 @@ from app.api.v2.projects.project_controller import router as projects_router_v2
 from app.api.v2.documents.documents_controller import router as documents_router_v2
 from app.api.v2.chat.chat_controller import router as chat_router_v2
 from app.api.v2.github.github_controller import router as github_router_v2
+from app.api.v2.auth.auth_controller import router as auth_router_v2
 from app.utils.api.router import TypedAPIRouter
+from app.utils.jwt import JWTHandler
+from fastapi import Depends
 
-projects_router = TypedAPIRouter(router=projects_router, prefix="", tags=["projects"])
-documents_router = TypedAPIRouter(router=documents_router, prefix="", tags=["documents"])
-chat_router = TypedAPIRouter(router=chat_router, prefix="", tags=["chat"])
-github_router = TypedAPIRouter(router=github_router, prefix="", tags=["github"])
-projects_routerV2 = TypedAPIRouter(router=projects_router_v2, prefix="/v2", tags=["projects"])
-documents_routerV2 = TypedAPIRouter(router=documents_router_v2, prefix="/v2", tags=["documents"])
-chat_routerV2 = TypedAPIRouter(router=chat_router_v2, prefix="/v2", tags=["chat"])
-github_routerV2 = TypedAPIRouter(router=github_router_v2, prefix="/v2", tags=["github"])
+projects_router = TypedAPIRouter(router=projects_router, prefix="", tags=["projects"], dependencies=[Depends(JWTHandler.decodeAccessToken)])
+documents_router = TypedAPIRouter(router=documents_router, prefix="", tags=["documents"], dependencies=[Depends(JWTHandler.decodeAccessToken)])
+chat_router = TypedAPIRouter(router=chat_router, prefix="", tags=["chat"], dependencies=[Depends(JWTHandler.decodeAccessToken)])
+github_router = TypedAPIRouter(router=github_router, prefix="", tags=["github"], dependencies=[Depends(JWTHandler.decodeAccessToken)])
+projects_routerV2 = TypedAPIRouter(router=projects_router_v2, prefix="/v2", tags=["projects"], dependencies=[Depends(JWTHandler.decodeAccessToken)])
+documents_routerV2 = TypedAPIRouter(router=documents_router_v2, prefix="/v2", tags=["documents"], dependencies=[Depends(JWTHandler.decodeAccessToken)])
+chat_routerV2 = TypedAPIRouter(router=chat_router_v2, prefix="/v2", tags=["chat"], dependencies=[Depends(JWTHandler.decodeAccessToken)])
+github_routerV2 = TypedAPIRouter(router=github_router_v2, prefix="/v2", tags=["github"], dependencies=[Depends(JWTHandler.decodeAccessToken)])
+auth_routerV2 = TypedAPIRouter(router=auth_router_v2, prefix="/v2", tags=["auth"], dependencies=[Depends(JWTHandler.decodeAccessToken)])
